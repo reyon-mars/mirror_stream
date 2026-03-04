5e0317f89736e1a85556c946a0ff56faf9011cfb
Reyon Rai <reyonrai50@gmail.com>
2026-03-04 17:40:09 +0545

    Implemented the type erased function and the thread_pool class.

--- include/utils/logger.hpp
+++ include/utils/logger.hpp
@@ -1,19 +1,19 @@
 #pragma once
-#include <string_view>
 #include <mutex>
+#include <string_view>
 
 namespace utils
 {
-    class logger
-    {
-    private:
-        static std::mutex log_mtx_;
+	class logger
+	{
+	private:
+		static std::mutex log_mtx_;
 
-        static void emit(std::string_view level, std::string_view msg, std::string_view color);
+		static void emit_log(std::string_view log_level, std::string_view log_msg, std::string_view log_color);
 
-    public:
-        static void log(std::string_view msg);
-        static void log_err(std::string_view err_msg);
-        static void log_warn(std::string_view warn_msg);
-    };
-}
+	public:
+		static void log(std::string_view msg);
+		static void log_err(std::string_view err_msg);
+		static void log_warn(std::string_view warn_msg);
+	};
+} // namespace utils
--- include/utils/thread_pool.hpp
+++ include/utils/thread_pool.hpp
@@ -1,45 +1,65 @@
 #pragma once
-#include <vector>
-#include <mutex>
 #include <condition_variable>
-#include <queue>
 #include <functional>
+#include <mutex>
+#include <queue>
 #include <thread>
+#include <vector>
 
-class thread_pool
+namespace utils
 {
-private:
-    std::vector<std::jthread> workers_;
-    std::queue<std::function<void()>> tasks_;
+	class thread_pool
+	{
+	private:
+		std::vector<std::jthread> workers_;
+		std::queue<std::function<void()>> tasks_;
 
-    std::mutex queue_mtx_;
-    std::condition_variable cv_;
-    bool stop_ = false;
+		std::mutex queue_mtx_;
+		std::condition_variable cv_;
+		bool stop_ = false;
 
-public:
-    thread_pool(size_t thread_count = (std::thread::hardware_concurrency() * 2))
-    {
-        workers_.reserve(thread_count);
-        for (size_t i = 0; i < thread_count; i++)
-        {
-            workers_.emplace_back([this]
-                                  {
-                while( true ){
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mtx_ );
-                        this->cv_.wait(lock, [this] {
-                            return this->stop_ || !this->tasks_.empty();
-                        });
+	public:
+		thread_pool(size_t thread_count = (std::thread::hardware_concurrency() * 2))
+		{
+			workers_.reserve(thread_count);
+			for (size_t i = 0; i < thread_count; i++)
+			{
+				workers_.emplace_back(
+					[this]
+					{
+						while (true)
+						{
+							std::function<void()> task;
+							{
+								std::unique_lock<std::mutex> lock(this->queue_mtx_);
+								this->cv_.wait(lock,
+											   [this]
+											   {
+												   return this->stop_ || !this->tasks_.empty();
+											   });
 
-                        if( this->stop_ && this->tasks_.empty()){
-                            return;
-                        }
-                        task = std::move( this->tasks_.front() );
-                        this->tasks_.pop();
-                    }
-                    task();
-                } });
-        }
-    }
-};
+								if (this->stop_ && this->tasks_.empty())
+								{
+									return;
+								}
+								task = std::move(this->tasks_.front());
+								this->tasks_.pop();
+							}
+							task();
+						}
+					});
+			}
+		}
+
+		void submit(std::function<void()> f)
+		{
+			{
+				std::lock_guard<std::mutex> lock(queue_mtx_);
+				this->tasks_.emplace(std::move(f));
+			}
+			cv_.notify_one();
+		}
+
+		~thread_pool() = default;
+	};
+} // namespace utils
