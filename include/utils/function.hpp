a1b8fef1b1fe547293d7f5998c969a293f1e1721
Reyon Rai <reyonrai50@gmail.com>
2026-03-04 22:06:36 +0545

    Completed the implementation of the type erased function wrapper.

--- include/utils/function.hpp
+++ include/utils/function.hpp
@@ -1,11 +1,16 @@
 #pragma once
+#include <concepts>
+#include <functional>
+#include <type_traits>
 #include <utility>
 
 namespace utils
 {
-	template <typename> class function;
+	template <typename>
+	class function;
 
-	template <typename R, typename... Args> class function<R(Args...)>
+	template <typename R, typename... Args>
+	class function<R(Args...)>
 	{
 
 	private:
@@ -16,11 +21,13 @@ namespace utils
 			virtual ~callable_base() = default;
 		};
 
-		template <typename Callable> struct callable_impl : callable_base
+		template <typename Callable>
+		struct callable_impl : callable_base
 		{
 			Callable m_callable_;
 
-			template <typename T> callable_impl(T&& c) : m_callable_(std::forward<T>(c))
+			template <typename T>
+			callable_impl(T&& c) : m_callable_(std::forward<T>(c))
 			{
 			}
 
@@ -40,6 +47,43 @@ namespace utils
 	public:
 		function() noexcept = default;
 
-		template
+		template <typename T>
+			requires(!std::same_as<std::decay_t<T>, function> && std::is_invocable_r_v<R, T, Args...>)
+
+		function(T&& f) : m_callable(new callable_impl<std::decay_t<T>>(std::forward<T>(f))){};
+
+		~function()
+		{
+			delete m_callable;
+		}
+
+		function(const function& other) : m_callable(other.m_callable ? other.m_callable->clone() : nullptr)
+		{
+		}
+
+		function(function&& other) noexcept : m_callable(other.m_callable)
+		{
+			other.m_callable = nullptr;
+		}
+
+		function& operator=(function other) noexcept
+		{
+			std::swap(m_callable, other.m_callable);
+			return *this;
+		}
+
+		R operator()(Args... args) const
+		{
+			if (!m_callable)
+			{
+				throw std::bad_function_call();
+			}
+			return m_callable->invoke(std::forward<Args>(args)...);
+		}
+
+		explicit operator bool() const
+		{
+			return m_callable != nullptr;
+		}
 	};
 } // namespace utils
